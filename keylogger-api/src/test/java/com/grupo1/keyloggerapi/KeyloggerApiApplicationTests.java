1c7ce084d8af1ed45ff77ab02625c117584f7483
José Suárez <74939658+jaSuarezGH@users.noreply.github.com>
2023-12-23 20:39:09 -0400

    Base de ejemplo

---
+++ keylogger-api/src/test/java/com/grupo1/keyloggerapi/KeyloggerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo1.keyloggerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyloggerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
